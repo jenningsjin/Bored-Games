8a669df5b573135404e3bc8dc88398cd7efa5ecb
jinjenni <jinjenni@umich.edu>
2015-02-21 11:05:53 -0500

    First commit

---
+++ Tic-Tac-Toe/tictactoe.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+
+struct space{
+	char val;
+	bool fill;
+
+	space() {
+		val = 'n';
+		fill = false;
+	}
+};
+
+class tictactoe {
+private:
+
+
+public:
+	space board[3][3];
+
+	validmove();
+	makemove();
+	victory();
+};
+
+
+
+
+//takes in the board 
+void solvedAI(){
+
+}
+
+int main () {
+	while (victory) {
+		break;
+	}
+
+	return 0;
+}
+
+
+
+tictactoe::validmove() {
+
+}
+
+
+
+int main() {
+	return 0;
+}
