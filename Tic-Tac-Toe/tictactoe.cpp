ccde4a71e6fa874c89167636e0da8c0f9d851ff0
jenningsjin <jinjenni@umich.edu>
2015-11-07 11:03:39 -0500

    added makefile, game can be played

--- Tic-Tac-Toe/tictactoe.cpp
+++ Tic-Tac-Toe/tictactoe.cpp
@@ -1,15 +1,16 @@
+#include <utility>
 #include "tictactoe.h"
 
 using namespace std;
 
 bool tictactoe::validmove(int x, int y) {
-	if (!board[x-1][y-1].fill) {
+	if (board[x-1][y-1].fill) {
 		return false;
 	}
 
 	if ( 
-		x >= 1 || x < 4 ||
-		y >= 1 || y < 4
+		x >= 1 && x < 4 &&
+		y >= 1 && y < 4
 		) {
 		return true;
 	}
@@ -18,18 +19,23 @@ bool tictactoe::validmove(int x, int y) {
 	return false;
 }
 
+
 void tictactoe::makemove(int x, int y, char input) {
 	if (validmove(x,y)) {
-		board[x-1][y-1].val = input;
+		cout << "move made" << endl;
+		board[x-1][y-1].val = input; 
 		board[x-1][y-1].fill = true;
+		return;
 	}
 
+	cout << "move not made" << endl;
 	return;
 }
 
 
 void tictactoe::printboard(){
 	for (int i=0; i < 3; i++) {
+		cout << '|';
 		for (int j =0; j<3; j++) {
 			cout << board[i][j].val << '|';
 		}
@@ -38,15 +44,30 @@ void tictactoe::printboard(){
 }
 
 bool tictactoe::victory() {
+	//check horizontal
+	//check vertical
+	//check 
 	return false;
 }
 
 
 int main() {
 	char move;
+	pair<int,int> playerMove;
 	tictactoe game;
-	
 	while (!game.victory()) {
+
+		game.printboard();
+		cout << "MAKE YOUR MOVE X: ";
+		move = 'x';
+		cin >> playerMove.first >> playerMove.second;
+		game.makemove(playerMove.first, playerMove.second, move);
+		
+		game.printboard();
+		cout << "MAKE YOUR MOVE O: ";
+		move = 'o';
+		cin >> playerMove.first >> playerMove.second;
+		game.makemove(playerMove.first, playerMove.second, move);
 	}
 	return 0;
 }
